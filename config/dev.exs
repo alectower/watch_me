f39c72a1c02e997ac942a61d95932081e87cf66f
Alec Tower <alectower@gmail.com>
2017-05-04 14:10:15 -0400

    Add separate database config file

--- config/dev.exs
+++ config/dev.exs
@@ -34,10 +34,4 @@ config :logger, :console, format: "[$level] $message\n"
 config :phoenix, :stacktrace_depth, 20
 
 # Configure your database
-config :watch_me, WatchMe.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "",
-  password: "",
-  database: "watch_me_dev",
-  hostname: "localhost",
-  pool_size: 10
+import_config "db.exs"
